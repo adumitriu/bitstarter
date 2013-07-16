374992e05758c13a7b4cd9ce395b73c3209020ac
Alexandra Dumitriu <adumitri@bu.edu>
2013-07-16 03:38:33 +0000

    modified index.html with wireframe design

--- web.js
+++ web.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World 2!');
   var index = fs.readFileSync('index.html');
   response.send(index.toString());
 });
